e3b4d97dd7a2e5af2e0e67a27c88b694b9f293d6
adithya <adithyayegiti93@gmail.com>
2026-04-02 14:23:53 +0530

    backready for rende

--- server/index.js
+++ server/index.js
@@ -112,6 +112,7 @@ app.post("/create-admin", async (req, res) => {
 app.get("/", (_, res) => {
     res.send("Backend is running");
 });
-app.listen(4000, () => {
-    console.log("Server running on http://localhost:4000");
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
